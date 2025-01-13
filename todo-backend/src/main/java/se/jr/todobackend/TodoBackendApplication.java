5af74919265eddae0c97613887d3e0e00f9e6ac5
johanromeo <johan_romeo123@live.se>
2025-01-13 21:10:38 +0100

    Initial commit

---
+++ todo-backend/src/main/java/se/jr/todobackend/TodoBackendApplication.java
@@ -0,0 +1,13 @@
+package se.jr.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackendApplication.class, args);
+	}
+
+}
